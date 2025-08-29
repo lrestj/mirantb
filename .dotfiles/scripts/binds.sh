62cb49dbeee3d9969772beeafad3ffcf25288434
LrestJ <rest@seznam.cz>
2025-08-29 23:46:18 +0200

    Nápověda klávesové zkratky

--- .dotfiles/scripts/binds.sh
+++ .dotfiles/scripts/binds.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 notify-send -u critical -c max -i ~/.dotfiles/icons/help.png "Klávesové zkratky" "$(cat .dotfiles/scripts/binds.txt)" 
+notify-send -u critical -c maxl -i ~/.dotfiles/icons/help.png "Klávesové zkratky" "$(cat .dotfiles/scripts/1binds.txt)" 
