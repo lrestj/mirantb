f090c05019a229c6da689aa56b6b411d22eeb936
LrestJ <rest@seznam.cz>
2025-08-29 21:41:13 +0200

    Fixes

---
+++ .dotfiles/scripts/binds.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+notify-send -u critical -c max -i ~/.dotfiles/icons/help.png "Klávesové zkratky" "$(cat .dotfiles/scripts/binds.txt)" 
--- .dotfiles/scripts/helpnotify.sh
+++ .dotfiles/scripts/helpnotify.sh
@@ -6,6 +6,6 @@ line1="\n Zavřít aktivní okno: +Q"
 line2="\nOkno na celou obrazovku a zpět: +F"
 line3="\nMenu aplikací:  "
 line4="\nMenu oblíbených: ALT+Y"
-hint="\n\n[kliknutím na mě zavři]"
+hint="\n Všechny zkratky:  ALT+N \n\n[kliknutím na mě zavři]"
 
 notify-send -t 35000 -c help -u critical -i $icon "$title" "$line1 $line2 $line3 $line4 $hint"
