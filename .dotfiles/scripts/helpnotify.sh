0fa8082b3e98c90f9d7a6bc5b4a1004e7aecbc7f
LrestJ <rest@seznam.cz>
2025-08-31 16:36:54 +0200

    Good

--- .dotfiles/scripts/helpnotify.sh
+++ .dotfiles/scripts/helpnotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 icon="/home/libor/.dotfiles/icons/help.png"
-title="Nápověda klávesové zkratky:"
+title="Základní klávesové zkratky:"
 
-notify-send -t 35000 -c help -u critical -i system-help "$title" "$(cat ~/.dotfiles/scripts/helpnotify.txt)"
+notify-send -t 35000 -c help -u critical -i dialog-information "$title" "$(cat ~/.dotfiles/scripts/helpnotify.txt)"
