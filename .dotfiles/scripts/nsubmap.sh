d4f8f3d6b318a56ee69d285c7e1872ff35ad28f9
LrestJ <rest@seznam.cz>
2025-08-31 13:04:37 +0200

    Fixes

--- .dotfiles/scripts/nsubmap.sh
+++ .dotfiles/scripts/nsubmap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-notify-send -u critical -c max -i ~/.dotfiles/icons/help.png "Nastavení" "$(cat ~/.dotfiles/scripts/nsubmap.txt)" 
+notify-send -u critical -c nas -i ~/.dotfiles/icons/help.png "Nastavení" "$(cat ~/.dotfiles/scripts/nsubmap.txt)" 
