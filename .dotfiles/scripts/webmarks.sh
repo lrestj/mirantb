867e3432cfe5a434e4da77904df49d0de928e456
LrestJ <rest@seznam.cz>
2025-08-31 16:17:26 +0200

    Fix

--- .dotfiles/scripts/webmarks.sh
+++ .dotfiles/scripts/webmarks.sh
@@ -2,12 +2,5 @@
 
 icon="/home/libor/.dotfiles/icons/brave.png"
 title="Otevřít webovou stránku..."
-line1="\n G  Gmail"
-line2="\n N  NixOS search"
-line3="\n P  Přehraj.to"
-line4="\n S  Sdílej.cz"
-line5="\n T  Titulky.cz"
-line6="\n F  Nerd Fonts"
-hint="\n\n[  CapsLk zavřít ]"
 
-notify-send -t 25000 -i $icon "$title" "$line1 $line2 $line3 $line4 $line5 $line6 $hint"
+notify-send -u critical -c nas -i $icon "$title" "$(cat ~/.dotfiles/scripts/webmarks.txt) "
