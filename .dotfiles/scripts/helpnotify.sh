3b6822e5df2ce88187598a1817e74d9ceff3a7b4
LrestJ <rest@seznam.cz>
2025-08-31 11:19:10 +0200

    Add helpnotify.txt

--- .dotfiles/scripts/helpnotify.sh
+++ .dotfiles/scripts/helpnotify.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 icon="/home/libor/.dotfiles/icons/help.png"
-title=" Nápověda klávesové zkratky:"
-line1="\n Zavřít aktivní okno: +Q"
-line2="\nOkno na celou obrazovku a zpět: +F"
-line3="\nMenu aplikací:  "
-line4="\nMenu oblíbených: ALT+Y"
-hint="\n Všechny zkratky:  ALT+N \n\n[kliknutím na mě zavři]"
+title="Nápověda klávesové zkratky:"
 
-notify-send -t 35000 -c help -u critical -i $icon "$title" "$line1 $line2 $line3 $line4 $hint"
+notify-send -t 35000 -c help -u critical -i $icon "$title" "$(cat ~/.dotfiles/scripts/helpnotify.txt)"
