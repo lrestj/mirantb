842405ed3e48dfd5b439e6ac189f6b69a3cf23fb
LrestJ <rest@seznam.cz>
2025-09-02 19:33:54 +0200

    Unmoumt google disk fixe

--- .dotfiles/scripts/clouds.sh
+++ .dotfiles/scripts/clouds.sh
@@ -5,8 +5,7 @@ OO="󰏊 Odpojit OneDrive"
 PG=" Připojit Google Disk"
 OG=" Odpojit Google Disk"
 
-selected=$(echo "$PO,$OO,$PG,
-$OG" | rofi -theme ~/.config/rofi/rofithemes/cloud.rasi -sep ',' -dmenu -p Úložiště)
+selected=$(echo "$PO,$OO,$PG,$OG," | rofi -theme ~/.config/rofi/rofithemes/cloud.rasi -sep ',' -dmenu -p Úložiště)
 
  if [ "$selected" = "$PO" ]; then
    source /home/libor/.dotfiles/scripts/onedrive.sh
