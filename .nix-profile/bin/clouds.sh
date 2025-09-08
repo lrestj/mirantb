4044b606c05fc8a74d46a88420aac6c803c88902
LrestJ <rest@seznam.cz>
2025-09-08 10:49:39 +0200

    Ricing scripts

--- .nix-profile/bin/clouds.sh
+++ .nix-profile/bin/clouds.sh
@@ -7,19 +7,36 @@ OG="  Odpojit Google Disk"
 
 selected=$(echo -e "$PO\n$OO\n$PG\n$OG" | fuzzel -d -p "Úložiště: " )
 
- if [ "$selected" = "$PO" ]; then
-   source onedrive.sh
- fi  
+ # if [ "$selected" = "$PO" ]; then
+ #   source onedrive.sh
+ # fi  
 
- if [ "$selected" = "$OO" ]; then
-   source umonedrive.sh
- fi
+ # if [ "$selected" = "$OO" ]; then
+ #   source umonedrive.sh
+ # fi
 
- if [ "$selected" = "$PG" ]; then
-   source googledrive.sh
- fi  
+ # if [ "$selected" = "$PG" ]; then
+ #   source googledrive.sh
+ # fi  
 
- if [ "$selected" = "$OG" ]; then
-   source umgoogledrive.sh
- fi
+ # if [ "$selected" = "$OG" ]; then
+ #   source umgoogledrive.sh
+ # fi
 
+ case $selected in
+     $PO)
+         source onedrive.sh
+     ;;
+     $OO)
+         source umonedrive.sh
+     ;;
+     $PG)
+         source googledrive.sh
+     ;;
+     $OG)
+         source umgoogledrive.sh
+     ;;
+     *)
+     notify-send -i im-kick-user "Přerušeno uživatelem"
+     ;;
+ esac
