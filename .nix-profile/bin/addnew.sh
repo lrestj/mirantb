a026016460224e46eb86ef39df4aaa478192c641
LrestJ <rest@seznam.cz>
2025-09-08 11:31:39 +0200

    Fixes

--- .nix-profile/bin/addnew.sh
+++ .nix-profile/bin/addnew.sh
@@ -3,18 +3,9 @@
 sel=$(fuzzel -d -p 'Jméno souboru:  ')
 file=~/.nix-profile/bin/$sel
 
-# if [ $sel="^/home/libor/.nix-profile/bin/$" ]; then
-#     notify-send -i data-error "Žádné jméno souboru" "Konec bez vytvoření souboru"
-# else   
-#     touch $file
-#     chmod +x $file
-#     echo "#!/bin/bash" > $file
-#     vim $file
-# fi
-
 case $sel in
     "")
-        notify-send -i data-error "Nezadali jste jméno" "Konec bez vytvoření souboru"
+        notify-send -i im-kick-user "Nezadali jste jméno" "Konec bez vytvoření souboru"
     ;;
     *)
         touch $file
