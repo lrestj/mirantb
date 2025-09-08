379d3eecd2630788461dad66165648b0244b19f2
LrestJ <rest@seznam.cz>
2025-09-08 11:15:11 +0200

    Fixes

--- .nix-profile/bin/addnew.sh
+++ .nix-profile/bin/addnew.sh
@@ -3,11 +3,23 @@
 sel=$(fuzzel -d -p 'Jméno souboru:  ')
 file=~/.nix-profile/bin/$sel
 
-# if [ $sel="" ]; then
+# if [ $sel="^/home/libor/.nix-profile/bin/$" ]; then
 #     notify-send -i data-error "Žádné jméno souboru" "Konec bez vytvoření souboru"
 # else   
-    touch $file
-    chmod +x $file
-    echo "#!/bin/bash" > $file
-    vim $file
+#     touch $file
+#     chmod +x $file
+#     echo "#!/bin/bash" > $file
+#     vim $file
 # fi
+
+case $sel in
+    "")
+        notify-send -i data-error "Žádné jméno souboru" "Konec bez vytvoření souboru"
+    ;;
+    *)
+        touch $file
+        chmod +x $file
+        echo "#!/bin/bash" > $file
+        vim $file
+    ;;
+esac
--- .nix-profile/bin/clouds.sh
+++ .nix-profile/bin/clouds.sh
@@ -6,37 +6,20 @@ PG="  Připojit Google Disk"
 OG="  Odpojit Google Disk"
 
 selected=$(echo -e "$PO\n$OO\n$PG\n$OG" | fuzzel -d -p "Úložiště: " )
-
- # if [ "$selected" = "$PO" ]; then
- #   source onedrive.sh
- # fi  
-
- # if [ "$selected" = "$OO" ]; then
- #   source umonedrive.sh
- # fi
-
- # if [ "$selected" = "$PG" ]; then
- #   source googledrive.sh
- # fi  
-
- # if [ "$selected" = "$OG" ]; then
- #   source umgoogledrive.sh
- # fi
-
- case $selected in
-     $PO)
-         source onedrive.sh
-     ;;
-     $OO)
-         source umonedrive.sh
-     ;;
-     $PG)
-         source googledrive.sh
-     ;;
-     $OG)
-         source umgoogledrive.sh
-     ;;
-     *)
-     notify-send -i im-kick-user "Přerušeno uživatelem"
-     ;;
- esac
+case $selected in
+    $PO)
+        source onedrive.sh
+    ;;
+    $OO)
+        source umonedrive.sh
+    ;;
+    $PG)
+        source googledrive.sh
+    ;;
+    $OG)
+        source umgoogledrive.sh
+    ;;
+    *)
+    notify-send -i im-kick-user "Přerušeno uživatelem"
+    ;;
+esac
