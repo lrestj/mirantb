f89178374a40303852c9f08d74a9fdf6e1a28942
LrestJ <rest@seznam.cz>
2025-09-09 12:37:13 +0200

    Fix

--- .local/bin/clouds.sh
+++ .local/bin/clouds.sh
@@ -5,7 +5,7 @@ OO="󰏊  Odpojit OneDrive"
 PG="  Připojit Google Disk"
 OG="  Odpojit Google Disk"
 
-selected=$(echo -e "$PO\n$OO\n$PG\n$OG" | fuzzel --lines 4 -d -p "Úložiště: " )
+selected=$(echo -e "$PO\n$OO\n$PG\n$OG" | fuzzel --minimal-lines -d -p "Úložiště: " )
 case $selected in
     $PO)
         source onedrive.sh
